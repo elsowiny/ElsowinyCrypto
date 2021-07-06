cb0892b61f0a6d29ddae53fba86b5342e4ecb085
Sherief Elsowiny <elsowiny@yahoo.com>
2021-07-05 20:23:12 -0400

    added gensis block

--- block.test.js
+++ block.test.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const { GENESIS_DATA } = require('./config');
 describe('Block', () => {
 
     const timestamp ='a-date';
@@ -14,5 +14,18 @@ describe('Block', () => {
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data)
 
+    });
+
+    describe('genesis()', () => {
+        const genesisBlock = Block.genesis();
+        console.log('genesis block', genesisBlock);
+
+        it('returns a Block instance' , () => {
+            expect(genesisBlock).toBeInstanceOf(Block);
+        });
+
+        it('returns the genesis',()=>{
+            expect(genesisBlock).toEqual(GENESIS_DATA);
+        })
     });
 });
