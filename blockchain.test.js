d618679f7a5e53c91be9d8188bb22feb2764be6d
Sherief Elsowiny <elsowiny@yahoo.com>
2021-07-09 23:27:39 -0400

    added chain validation tests

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,10 +2,15 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-    const blockchain = new Blockchain();
+    let blockchain;
+
+    beforeEach(() => {
+        blockchain = new Blockchain();
+    });
+
     it('should contain a `chain` Array instance', () =>{
         expect(blockchain.chain).toBeInstanceOf(Array);
-    } );
+    });
 
     it('should start with the genesis block', () =>{
         expect(blockchain.chain[0]).toEqual(Block.genesis());
@@ -17,4 +22,56 @@ describe('Blockchain', () => {
         blockchain.addBlock({data: newData});
         expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
     });
+
+    
+    
+    
+    describe('isValidChain()', () =>{
+        beforeEach(() => {
+            blockchain.addBlock({data: 'Elsowiny'});
+            blockchain.addBlock({data: 'Sherief'});
+            blockchain.addBlock({data: 'yoloSwag'});
+        });
+
+        describe('when the chain does not start with the genesis block', () =>{
+            it('should return false', () =>{
+               blockchain.chain[0] = {data: 'hey fake'};
+               expect(Blockchain.isValidChain(blockchain.chain)).toBeFalse();
+            });
+        });
+
+        describe('when the chain starts with the genesis block', () =>{
+                describe('and a lastHash reference changed' , () =>{
+                    it('should return false', () =>{
+                      
+
+                        // tamper data
+                        blockchain[2].lastHash = 'yoloSwaggerHacker';
+                        expect(Blockchain.isValidChain(blockchain.chain)).toBeFalse();
+                    });
+                });
+        });
+
+        describe('and the chain contains a block with an invalid field',()=>{
+             it('should return false', () =>{
+                
+
+                // tamper data
+                blockchain.chain[2].data = 'yoloSwaggerHacker';
+                expect(Blockchain.isValidChain(blockchain.chain)).toBeFalse();
+
+             });
+        });
+        
+        describe('and the chain does not contain a any invalid data',()=>{
+            it('should return true', () =>{
+             
+
+                expect(Blockchain.isValidChain(blockchain.chain)).toBeTrue();
+            });
+        });
+
+    });
+
+        
 });
