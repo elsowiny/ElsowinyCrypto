dd65eb55f8546f42b141c0144a3c5400affc6f64
Sherief Elsowiny <elsowiny@yahoo.com>
2021-07-18 18:38:24 -0400

    tweaked the hashing algorithm to adjust based on the leading 0's of the binary version of the hex. this allows for our mining algo to mine at a rate we specified in the config file. also allows for better difficutly in mining

--- average-work.js
+++ average-work.js
@@ -3,7 +3,7 @@ const Blockchain = require('./blockchain');
 const blockchain = new Blockchain();
 
 blockchain.addBlock({data: 'initial'});
-
+console.log('first block', blockchain.chain[blockchain.chain.length - 1]);
 let prevTimestamp, nextTimestamp, nextBlock, timeDiff, averageTime;
 
 const times = [];
@@ -18,6 +18,6 @@ for (let i=0; i<10000; i++){
     times.push(timeDiff);
 
     averageTime = times.reduce((total, num) => (total + num))/times.length;
-    console.log(`Time to mine block: ${timeDiff}ms. Diffculty: ${nextBlock.difficulty}.
-        average time to mine: ${averageTime}ms`)
+    console.log(`Block# ${i} Time to mine block: ${timeDiff}ms. Diffculty: ${nextBlock.difficulty}.
+       average time to mine: ${averageTime}ms`)
 }
--- block.js
+++ block.js
@@ -1,5 +1,6 @@
 const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
+const hexToBinary = require('hex-to-binary');
 
 class Block{
      constructor({timestamp, lastHash, hash, data, nonce, difficulty}){
@@ -29,7 +30,7 @@ class Block{
 
             difficulty = Block.adjustDifficulty({originalBlock: lastBlock, timestamp});
             hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
-        } while(hash.substring(0, difficulty) !== '0'.repeat(difficulty));
+        } while(hexToBinary(hash).substring(0, difficulty) !== '0'.repeat(difficulty));
 
         return new this({
             timestamp,
@@ -45,7 +46,7 @@ class Block{
 
     static adjustDifficulty({originalBlock, timestamp}){
         const {difficulty} = originalBlock;
-        
+
         if(difficulty < 1){return 1;}
 
         const difference = timestamp - originalBlock.timestamp;
